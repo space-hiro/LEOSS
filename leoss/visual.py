2701fca2e3c1f36b6b8d5b298673a67328a537a2
space-hiro <144877753+space-hiro@users.noreply.github.com>
2024-03-08 21:32:04 +0800

    Update visual.py
    version 0.2.18
    
    added new feature sliderAttitutdeTrack() - a mixed of animatedAttitudeTrack and the basic attitudeTrack. This one will allow more user input to use a slider and select what timestep to show on the plot.
    
    The update on the plot is very slow.
    All tests passed so far

--- leoss/visual.py
+++ leoss/visual.py
@@ -8,6 +8,7 @@ from matplotlib.transforms import offset_copy
 from matplotlib import gridspec
 from matplotlib.widgets import Cursor
 from matplotlib.widgets import Button
+from matplotlib.widgets import Slider
 
 import matplotlib.ticker as mticker
 from cartopy.mpl.gridliner import LONGITUDE_FORMATTER, LATITUDE_FORMATTER
@@ -380,6 +381,371 @@ def animatedAttitudeTrack(recorder: Recorder, sample: int = 0, saveas: str = "mp
         anim.save("Attitudetrack.gif", writer='pillow', fps=30)
 
     plt.close()
+
+def sliderAttitudeTrack(recorder: Recorder, frameRef: str = 'Inertial'):
+    global frame
+    frame = 0
+
+    df = pd.DataFrame.from_dict(recorder.dataDict)
+
+    spacecraft = recorder.attachedTo
+    system = spacecraft.system
+
+    xs =spacecraft.size.x/min(spacecraft.size)
+    ys =spacecraft.size.y/min(spacecraft.size)
+    zs =spacecraft.size.z/min(spacecraft.size)
+
+    ratio = max(spacecraft.size)/min(spacecraft.size)
+
+    States      = [ item for item in df['State'].values.tolist()[:] ]
+    Positions   = [ item.position for item in df['State'].values.tolist()[:] ]
+    Velocities  = [ item.velocity for item in df['State'].values.tolist()[:] ]
+    Quaternions = [ item.quaternion for item in df['State'].values.tolist()[:] ]
+    Bodyrates   = [ item.bodyrate*R2D for item in df['State'].values.tolist()[:] ]
+    Datetimes   = [ item for item in df['Datetime'] ][:]
+    Times       = [ (item - system.datetime0).total_seconds() for item in df['Datetime'][:] ]
+
+    fig = plt.figure(figsize=(18,9))
+    fig.tight_layout()
+    ax1 = fig.add_subplot(3,2,2)
+    ax2 = fig.add_subplot(3,2,4)
+    ax3 = fig.add_subplot(3,2,6)
+    ax4 = fig.add_subplot(2,4,1, projection='3d')
+    ax5 = fig.add_subplot(2,4,2, projection='3d')
+    ax6 = fig.add_subplot(2,4,5, projection='3d')
+    ax7 = fig.add_subplot(2,4,6, projection='3d')
+
+    QuatW = [ q.w for q in Quaternions ]
+    QuatX = [ q.x for q in Quaternions ]
+    QuatY = [ q.y for q in Quaternions ]
+    QuatZ = [ q.z for q in Quaternions ]
+    RateX = [ r.x for r in Bodyrates ]
+    RateY = [ r.y for r in Bodyrates ]
+    RateZ = [ r.z for r in Bodyrates ]
+
+    Eulers = [ q.YPR_toRPY_vector() for q in Quaternions ]
+    Roll  = [ item.x*R2D for item in Eulers ]
+    Pitch = [ item.y*R2D for item in Eulers ]
+    Yaw   = [ item.z*R2D for item in Eulers ]
+
+    Matrices = [ q.toMatrix().transpose() for q in Quaternions ]
+
+    ln1, = ax1.plot([],[], label='q0')
+    ln2, = ax1.plot([],[], label='q1')
+    ln3, = ax1.plot([],[], label='q2')
+    ln4, = ax1.plot([],[], label='q3')
+
+    ln5, = ax2.plot([],[], label='X')
+    ln6, = ax2.plot([],[], label='Y')
+    ln7, = ax2.plot([],[], label='Z')
+
+    ln8, = ax3.plot([],[], label='roll')
+    ln9, = ax3.plot([],[], label='pitch')
+    ln10, = ax3.plot([],[], label='yaw')
+
+    ln11, = ax4.plot([],[],[])
+    ln12, = ax4.plot([],[],[])
+    ln13, = ax4.plot([],[],[])
+    ln14, = ax4.plot([],[],[])
+
+    ln15, = ax5.plot([],[],[])
+    ln16, = ax5.plot([],[],[])
+    ln17, = ax5.plot([],[],[])
+
+    ln18, = ax6.plot([],[],[])
+    ln19, = ax6.plot([],[],[])
+    ln20, = ax6.plot([],[],[])
+
+    ln21, = ax7.plot([],[],[])
+    ln22, = ax7.plot([],[],[])
+    ln23, = ax7.plot([],[],[])
+    
+    ax1.grid(visible=True)
+    ax2.grid(visible=True)
+    ax3.grid(visible=True)
+
+    ax4.view_init(30,20)
+    ax5.view_init(0,0)
+    ax6.view_init(0,90)
+    ax7.view_init(90,0)
+
+    ax1.set_title(f'Quaternion', fontsize=10)
+    ax2.set_title(f'Body Rates (deg/s)', fontsize=10)
+    ax3.set_title(f'Euler Angles (deg)', fontsize=10)
+
+    def init():
+        ax1.set_ylim(-1.1,1.1)
+        ax3.set_ylim(-190,190)
+
+        for axis in ax5.xaxis, ax6.yaxis, ax7.zaxis:
+            axis.set_label_position('none')
+            axis.set_ticks_position('none')
+
+        ax6.zaxis.set_label_position('upper')
+        ax6.zaxis.set_ticks_position('upper')    
+
+        plt.subplots_adjust(wspace=0.1, hspace=0.3, left=None, right=None, bottom=0.2)   
+
+    def update(input):
+        global frame 
+        frame = int(input)
+        frame = int(frame)
+        datetimeText = Datetimes[frame].strftime("%Y-%m-%d %H:%M:%S.%f")
+        plt.suptitle(f'{spacecraft.name}\n{datetimeText}', fontname='monospace')
+
+        ln1.set_data(Times[0: frame], QuatW[0: frame])
+        ln2.set_data(Times[0: frame], QuatX[0: frame])
+        ln3.set_data(Times[0: frame], QuatY[0: frame])
+        ln4.set_data(Times[0: frame], QuatZ[0: frame])
+        ln5.set_data(Times[0: frame], RateX[0: frame])
+        ln6.set_data(Times[0: frame], RateY[0: frame])
+        ln7.set_data(Times[0: frame], RateZ[0: frame])
+        ln8.set_data(Times[0: frame], Roll[0: frame])
+        ln9.set_data(Times[0: frame], Pitch[0: frame])
+        ln10.set_data(Times[0: frame], Yaw[0: frame])    
+
+        ln1.set_label("q0 = "+str('%+.4F' % QuatW[frame]))
+        ln2.set_label("q1 = "+str('%+.4F' % QuatX[frame]))
+        ln3.set_label('q2 = '+str('%+.4F' % QuatY[frame]))
+        ln4.set_label('q3 = '+str('%+.4F' % QuatZ[frame]))
+        ax1.legend(loc='lower left', prop={'family':'monospace'}, ncol=4)    
+
+        ln5.set_label("X = "+str('%+.4F' % RateX[frame]))
+        ln6.set_label("Y = "+str('%+.4F' % RateY[frame]))
+        ln7.set_label('Z = '+str('%+.4F' % RateZ[frame]))
+        ax2.legend(loc='lower left', prop={'family':'monospace'}, ncol=3)   
+
+        ln8.set_label("Roll = "+str('%+.4F' % Roll[frame]))
+        ln9.set_label("Pitch = "+str('%+.4F' % Pitch[frame]))
+        ln10.set_label('Yaw = '+str('%+.4F' % Yaw[frame]))
+        ax3.legend(loc='lower left', prop={'family':'monospace'}, ncol=3)  
+
+        xaxis = Vector(1,0,0)
+        yaxis = Vector(0,1,0)
+        zaxis = Vector(0,0,1)
+        maxis = Matrices[frame] * spacecraft.inertia*Bodyrates[frame]
+
+        Rotation = Matrices[frame]
+        maxisLine = maxis.normalize() * ratio * 2
+
+        if frameRef == 'Momentum':
+            maxisZ = maxis.normalize()
+            if maxisZ.cross(zaxis).magnitude() == 0:
+                maxisX = maxisZ.cross(xaxis).normalize()
+            else:
+                maxisX = maxisZ.cross(zaxis).normalize()
+            maxisY = maxisZ.cross(maxisX).normalize()
+            MomentumRotation = Matrix(maxisX, maxisY,maxisZ)
+
+            Rotation = MomentumRotation.transpose() * Matrices[frame]
+            maxisLine = MomentumRotation.transpose() * maxis.normalize() * ratio * 2
+
+        if frameRef == 'Orbit':
+            Raxis = Positions[frame].normalize()
+            Vaxis = Velocities[frame].normalize()
+            Haxis = Raxis.cross(Vaxis)
+            Taxis = Haxis.cross(Raxis)
+            OrbitRotation = Matrix(Taxis, -1*Haxis, -1*Raxis)
+
+            Rotation = OrbitRotation.transpose() * Matrices[frame]
+            maxisLine = OrbitRotation.transpose() * maxis.normalize() * ratio * 2
+
+        xaxis = Rotation * xaxis * xs * 2
+        yaxis = Rotation * yaxis * ys * 2
+        zaxis = Rotation * zaxis * zs * 2
+
+        xFaceP1 = Rotation * Vector( xs,-ys, zs)
+        xFaceP2 = Rotation * Vector( xs, ys, zs)
+        xFaceP3 = Rotation * Vector( xs, ys,-zs)
+        xFaceP4 = Rotation * Vector( xs,-ys,-zs)
+
+        nxFaceP1 = Rotation * Vector(-xs, ys, zs)
+        nxFaceP2 = Rotation * Vector(-xs,-ys, zs)
+        nxFaceP3 = Rotation * Vector(-xs,-ys,-zs)
+        nxFaceP4 = Rotation * Vector(-xs, ys,-zs)
+
+        yFaceP1 = Rotation * Vector( xs, ys, zs)
+        yFaceP2 = Rotation * Vector(-xs, ys, zs)
+        yFaceP3 = Rotation * Vector(-xs, ys,-zs)
+        yFaceP4 = Rotation * Vector( xs, ys,-zs)
+
+        nyFaceP1 = Rotation * Vector(-xs,-ys, zs)
+        nyFaceP2 = Rotation * Vector( xs,-ys, zs)
+        nyFaceP3 = Rotation * Vector( xs,-ys,-zs)
+        nyFaceP4 = Rotation * Vector(-xs,-ys,-zs)
+
+        zFaceP1 = Rotation * Vector(-xs,-ys, zs)
+        zFaceP2 = Rotation * Vector(-xs, ys, zs)
+        zFaceP3 = Rotation * Vector( xs, ys, zs)
+        zFaceP4 = Rotation * Vector( xs,-ys, zs)
+
+        nzFaceP1 = Rotation * Vector(-xs,-ys,-zs)
+        nzFaceP2 = Rotation * Vector(-xs, ys,-zs)
+        nzFaceP3 = Rotation * Vector( xs, ys,-zs)
+        nzFaceP4 = Rotation * Vector( xs,-ys,-zs)
+
+        xFaceX = np.array([xFaceP1.x, xFaceP2.x, xFaceP3.x, xFaceP4.x])
+        xFaceY = np.array([xFaceP1.y, xFaceP2.y, xFaceP3.y, xFaceP4.y])
+        xFaceZ = np.array([xFaceP1.z, xFaceP2.z, xFaceP3.z, xFaceP4.z])
+
+        nxFaceX = np.array([nxFaceP1.x, nxFaceP2.x, nxFaceP3.x, nxFaceP4.x])
+        nxFaceY = np.array([nxFaceP1.y, nxFaceP2.y, nxFaceP3.y, nxFaceP4.y])
+        nxFaceZ = np.array([nxFaceP1.z, nxFaceP2.z, nxFaceP3.z, nxFaceP4.z])
+
+        yFaceX = np.array([yFaceP1.x, yFaceP2.x, yFaceP3.x, yFaceP4.x])
+        yFaceY = np.array([yFaceP1.y, yFaceP2.y, yFaceP3.y, yFaceP4.y])
+        yFaceZ = np.array([yFaceP1.z, yFaceP2.z, yFaceP3.z, yFaceP4.z])
+
+        nyFaceX = np.array([nyFaceP1.x, nyFaceP2.x, nyFaceP3.x, nyFaceP4.x])
+        nyFaceY = np.array([nyFaceP1.y, nyFaceP2.y, nyFaceP3.y, nyFaceP4.y])
+        nyFaceZ = np.array([nyFaceP1.z, nyFaceP2.z, nyFaceP3.z, nyFaceP4.z])
+
+        zFaceX = np.array([zFaceP1.x, zFaceP2.x, zFaceP3.x, zFaceP4.x])
+        zFaceY = np.array([zFaceP1.y, zFaceP2.y, zFaceP3.y, zFaceP4.y])
+        zFaceZ = np.array([zFaceP1.z, zFaceP2.z, zFaceP3.z, zFaceP4.z])
+
+        nzFaceX = np.array([nzFaceP1.x, nzFaceP2.x, nzFaceP3.x, nzFaceP4.x])
+        nzFaceY = np.array([nzFaceP1.y, nzFaceP2.y, nzFaceP3.y, nzFaceP4.y])
+        nzFaceZ = np.array([nzFaceP1.z, nzFaceP2.z, nzFaceP3.z, nzFaceP4.z])
+
+        xFace = np.zeros([4,3])
+        xFace[:,0] = xFaceX
+        xFace[:,1] = xFaceY
+        xFace[:,2] = xFaceZ
+
+        nxFace = np.zeros([4,3])
+        nxFace[:,0] = nxFaceX
+        nxFace[:,1] = nxFaceY
+        nxFace[:,2] = nxFaceZ
+
+        yFace = np.zeros([4,3])
+        yFace[:,0] = yFaceX
+        yFace[:,1] = yFaceY
+        yFace[:,2] = yFaceZ
+
+        nyFace = np.zeros([4,3])
+        nyFace[:,0] = nyFaceX
+        nyFace[:,1] = nyFaceY
+        nyFace[:,2] = nyFaceZ
+
+        zFace = np.zeros([4,3])
+        zFace[:,0] = zFaceX
+        zFace[:,1] = zFaceY
+        zFace[:,2] = zFaceZ
+
+        nzFace = np.zeros([4,3])
+        nzFace[:,0] = nzFaceX
+        nzFace[:,1] = nzFaceY
+        nzFace[:,2] = nzFaceZ
+
+        ax4.clear()
+        ax4.set_xlim(-ratio,ratio)
+        ax4.set_ylim(-ratio,ratio)
+        ax4.set_zlim(-ratio,ratio)
+        ax4.grid(False)
+        ax4.plot([0,xaxis.x],[0,xaxis.y],[0,xaxis.z],c='red',   linewidth=1.0, zorder=5, linestyle='--')
+        ax4.plot([0,yaxis.x],[0,yaxis.y],[0,yaxis.z],c='green', linewidth=1.0, zorder=5, linestyle='--')
+        ax4.plot([0,zaxis.x],[0,zaxis.y],[0,zaxis.z],c='blue',  linewidth=1.0, zorder=5, linestyle='--')
+        ax4.plot([0,maxisLine.x],[0,maxisLine.y],[0,maxisLine.z],c='orange',linewidth=1.0, zorder=5, linestyle='-')
+        ax4.add_collection(Poly3DCollection([xFace, yFace, zFace], facecolors='cyan', linewidths=1, edgecolors='k', alpha=.50))
+        ax4.add_collection(Poly3DCollection([nxFace, nyFace, nzFace], facecolors='cyan', linewidths=1, edgecolors='k', alpha=.50))
+
+        scale = 2.0
+        ax5.clear()
+        ax5.set_xlim(-ratio*scale,ratio*scale)
+        ax5.set_ylim(-ratio*scale,ratio*scale)
+        ax5.set_zlim(-ratio*scale,ratio*scale)
+        ax5.grid(False)
+        ax5.plot([0,xaxis.x],[0,xaxis.y],[0,xaxis.z],c='red',   linewidth=1.0, zorder=5, linestyle='--')
+        ax5.plot([0,yaxis.x],[0,yaxis.y],[0,yaxis.z],c='green', linewidth=1.0, zorder=5, linestyle='--')
+        ax5.plot([0,zaxis.x],[0,zaxis.y],[0,zaxis.z],c='blue',  linewidth=1.0, zorder=5, linestyle='--')
+        ax5.add_collection(Poly3DCollection([xFace, yFace, zFace], facecolors='cyan', linewidths=1, edgecolors='k', alpha=.50))
+        ax5.add_collection(Poly3DCollection([nxFace, nyFace, nzFace], facecolors='cyan', linewidths=1, edgecolors='k', alpha=.50))
+
+        ax6.clear()
+        ax6.set_xlim(-ratio*scale,ratio*scale)
+        ax6.set_ylim(-ratio*scale,ratio*scale)
+        ax6.set_zlim(-ratio*scale,ratio*scale)
+        ax6.grid(False)
+        ax6.plot([0,xaxis.x],[0,xaxis.y],[0,xaxis.z],c='red',   linewidth=1.0, zorder=5, linestyle='--')
+        ax6.plot([0,yaxis.x],[0,yaxis.y],[0,yaxis.z],c='green', linewidth=1.0, zorder=5, linestyle='--')
+        ax6.plot([0,zaxis.x],[0,zaxis.y],[0,zaxis.z],c='blue',  linewidth=1.0, zorder=5, linestyle='--')
+        ax6.add_collection(Poly3DCollection([xFace, yFace, zFace], facecolors='cyan', linewidths=1, edgecolors='k', alpha=.50))
+        ax6.add_collection(Poly3DCollection([nxFace, nyFace, nzFace], facecolors='cyan', linewidths=1, edgecolors='k', alpha=.50))
+
+        ax7.clear()
+        ax7.set_xlim(-ratio*scale,ratio*scale)
+        ax7.set_ylim(-ratio*scale,ratio*scale)
+        ax7.set_zlim(-ratio*scale,ratio*scale)
+        ax7.grid(False)
+        ax7.plot([0,xaxis.x],[0,xaxis.y],[0,xaxis.z],c='red',   linewidth=1.0, zorder=5, linestyle='--')
+        ax7.plot([0,yaxis.x],[0,yaxis.y],[0,yaxis.z],c='green', linewidth=1.0, zorder=5, linestyle='--')
+        ax7.plot([0,zaxis.x],[0,zaxis.y],[0,zaxis.z],c='blue',  linewidth=1.0, zorder=5, linestyle='--')
+        ax7.add_collection(Poly3DCollection([xFace, yFace, zFace], facecolors='cyan', linewidths=1, edgecolors='k', alpha=.50))
+        ax7.add_collection(Poly3DCollection([nxFace, nyFace, nzFace], facecolors='cyan', linewidths=1, edgecolors='k', alpha=.50))
+
+        frameText = ''
+        if frameRef == 'Inertial':
+            frameText = 'ECIF'
+        if frameRef == 'Momentum':
+            frameText = 'Angular Momentum Vector'
+        if frameRef == 'Orbit':
+            frameText = 'LVLH Earth Pointing'
+
+        ax4.set_title(f'3D View\nFrame = {frameText}', fontsize=10, fontname='monospace')
+        ax5.set_title(f'Perspective Along +X', fontsize=10, fontname='monospace', y=-0.01)
+        ax6.set_title(f'Perspective Along +Y', fontsize=10, fontname='monospace', y=-0.01)
+        ax7.set_title(f'Perspective Along +Z', fontsize=10, fontname='monospace', y=-0.01)
+
+        if frame > 0:
+            ax1.set_xlim(Times[0]-1, Times[frame]+10)
+            ax2.set_xlim(Times[0]-1, Times[frame]+10)
+            ax3.set_xlim(Times[0]-1, Times[frame]+10)
+            maxV = 0
+            for rate in Bodyrates[0: frame]:
+                lis = abs(np.array([rate.x, rate.y, rate.z]))
+                if max(lis) > maxV:
+                    maxV = max(lis)
+
+            ax2.set_ylim(-1.5*maxV,1.5*maxV)
+
+    init()
+    update(0)
+
+    axtime = fig.add_axes([0.2, 0.1, 0.65, 0.03])
+    time_slider = Slider(
+        ax      = axtime,
+        label   = "TimeStep",
+        valmin  = 0,
+        valmax  = len(Times)-1,
+        valinit = 0,
+        valstep = 1,
+        color   = 'green'
+    )
+    forwardax = fig.add_axes([0.55, 0.025, 0.1, 0.04])
+    backwardax = fig.add_axes([0.45, 0.025, 0.1, 0.04])
+    buttonF = Button(forwardax, '>', hovercolor='0.975')
+    buttonB = Button(backwardax, '<', hovercolor='0.975')
+
+    def forward(event):
+        global frame
+        frame = frame + 1
+        update(frame)
+        time_slider.set_val(frame)
+
+    def backward(event):
+        global frame
+        frame = frame - 1
+        update(frame)
+        time_slider.set_val(frame)
+
+    buttonF.on_clicked(forward)
+    buttonB.on_clicked(backward)
+
+
+    time_slider.on_changed(update)
+
+    plt.show()
 
 def attitudeTrack(recorder: Recorder):
 
